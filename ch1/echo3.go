a5d8c5d5c155c1e93b38efde24e04b37b0f527dd
Lee Gi-Hwan <50398872+Gyanni@users.noreply.github.com>
2023-02-22 13:34:22 +0900

    Update echo3.go

--- ch1/echo3.go
+++ ch1/echo3.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	fmt.Println(strings.Join(os.Args[1:], " ")) // output : hi hello good
 	fmt.Println(os.Args[1:])                    // output : [hi hello good]
-	fmt.Println(os.Args[0:])                    // Exercise 1.1
+	fmt.Println(os.Args[0:])       
 	// output : [/private/var/folders/y3/gzcc6kg97n7grfc26115w6dh0000gn/T/GoLand/___go_build_echo3_go hi hello good]
 	// os.Args[0]에는 build 명령어가 매개변수로 입력된다.
 }
